20a4fe7481a4b52ecf85b7f62bcff84403ccc1fe
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-08 19:10:29 +0530

    Index Updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -159,12 +159,3 @@
 
 
 
-if("serviceWorker" in navigator){
-  navigator.serviceWorker.register("sw.js").then(registration => {
-      console.log("SW Registered.");
-      console.log(registration);
-  }).catch(err => {
-      console.log("SW Failed");
-      console.log(error);
-  });
-}
