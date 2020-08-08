b80a674e6242edcb54cea3152a4ba3bc41431bc8
Dhruv Desai <dhruvshah28@gmail.com>
2020-08-08 20:20:25 +0530

    Portfolio Updated

--- assets/js/github.js
+++ assets/js/github.js
@@ -44,20 +44,20 @@ fetch("https://api.github.com/users/desaidhruv/repos")
           fill-rule="evenodd"
           d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
         ></path>
-      </svg>   ${data[17].name}`
-        document.getElementById("card-text3").innerHTML = `${data[17].description}`
+      </svg>   ${data[18].name}`
+        document.getElementById("card-text3").innerHTML = `${data[18].description}`
         var element = document.getElementById("card-link3")
         element.classList.add("card-link3");
         document.getElementById("card-link3").innerHTML = `
-            <a href="${data[17].html_url}" class="btn">View</a>
+            <a href="${data[18].html_url}" class="btn">View</a>
         `
         document.getElementById("title4").innerHTML = `<svg  aria-hidden="true" className="octicon" height="20" role="img" viewBox="0 0 12 16" width="14" className="repo-svg">
         <path
           fill-rule="evenodd"
           d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
         ></path>
-      </svg>   ${data[21].name}`
-        document.getElementById("card-text4").innerHTML = `${data[21].description}`
+      </svg>   ${data[22].name}`
+        document.getElementById("card-text4").innerHTML = `${data[22].description}`
         var element = document.getElementById("card-link4")
         element.classList.add("card-link4");
         document.getElementById("card-link4").innerHTML = `
@@ -68,8 +68,8 @@ fetch("https://api.github.com/users/desaidhruv/repos")
           fill-rule="evenodd"
           d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
         ></path>
-      </svg>   ${data[19].name}`
-        document.getElementById("card-text5").innerHTML = `${data[19].description}`
+      </svg>   ${data[20].name}`
+        document.getElementById("card-text5").innerHTML = `${data[20].description}`
         var element = document.getElementById("card-link5")
         element.classList.add("card-link5");
         document.getElementById("card-link5").innerHTML = `
