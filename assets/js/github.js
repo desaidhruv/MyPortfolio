b4a8695600faf8f054df1b64158053f135ec4cee
Dhruv Desai <dhruvshah28@gmail.com>
2021-02-03 15:13:28 +0530

    Preloader Added

--- assets/js/github.js
+++ assets/js/github.js
@@ -97,8 +97,8 @@ fetch("https://api.github.com/users/desaidhruv/repos")
           fill-rule="evenodd"
           d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
         ></path>
-      </svg>   ${data[16].name}`
-        document.getElementById("card-text4").innerHTML = `${data[16].description}`
+      </svg>   ${data[14].name}`
+        document.getElementById("card-text4").innerHTML = `${data[14].description}`
         var element = document.getElementById("card-link4")
         element.classList.add("card-link4");
         document.getElementById("card-link4").innerHTML = `
@@ -111,7 +111,7 @@ fetch("https://api.github.com/users/desaidhruv/repos")
               border-width: 5px;
               height: 25px;
               width: 25px;"></div>
-              <p style="margin:0;padding-left:10px;">${data[16].language}</p>
+              <p style="margin:0;padding-left:10px;">${data[14].language}</p>
             </div>
         `
         document.getElementById("title5").innerHTML = `<svg  aria-hidden="true" className="octicon" height="20" role="img" viewBox="0 0 12 16" width="14" className="repo-svg">
@@ -119,8 +119,8 @@ fetch("https://api.github.com/users/desaidhruv/repos")
           fill-rule="evenodd"
           d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"
         ></path>
-      </svg>   ${data[14].name}`
-        document.getElementById("card-text5").innerHTML = `${data[14].description}`
+      </svg>   ${data[10].name}`
+        document.getElementById("card-text5").innerHTML = `${data[10].description}`
         var element = document.getElementById("card-link5")
         element.classList.add("card-link5");
         document.getElementById("card-link5").innerHTML = `
@@ -133,7 +133,7 @@ fetch("https://api.github.com/users/desaidhruv/repos")
               border-width: 5px;
               height: 25px;
               width: 25px;"></div>
-              <p style="margin:0;padding-left:10px;">${data[14].language}</p>
+              <p style="margin:0;padding-left:10px;">${data[10].language}</p>
             </div>
         `
 
